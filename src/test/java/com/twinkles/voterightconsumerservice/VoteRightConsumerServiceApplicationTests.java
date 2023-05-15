3e22e958d63580ca88a20f3c9ce9c0297a074c96
Adewale103 <adeyinkawale13@gmail.com>
2023-05-15 08:15:45 +0100

    "started with kafka config"

---
+++ src/test/java/com/twinkles/voterightconsumerservice/VoteRightConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twinkles.voterightconsumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteRightConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
